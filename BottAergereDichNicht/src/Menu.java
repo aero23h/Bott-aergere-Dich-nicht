43c983728ddaafc7aad7d406ccae5e3325123b00
alex <alex@GamingTower-Alex.fritz.box>
2022-05-24 19:50:21 +0200

    Add first CSV Test

--- BottAergereDichNicht/src/Menu.java
+++ BottAergereDichNicht/src/Menu.java
@@ -1,8 +1,14 @@
 import java.io.IOException;
 
+import com.opencsv.exceptions.CsvException;
+
 public class Menu {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, CsvException {
 		Saves s = new Saves();
-		s.saveCSV();
+		//s.saveCSV();
+		s.loadCSV();
+		
+		Game g = new Game();
+		g.printBoard();
 	}
 }
--- BottAergereDichNicht/src/Saves.java
+++ BottAergereDichNicht/src/Saves.java
@@ -1,6 +1,9 @@
 import com.opencsv.*;
+import com.opencsv.exceptions.CsvException;
+
 import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Saves {
 	private String filePath;
@@ -19,7 +22,15 @@ public class Saves {
 		CSVWriter writer = new CSVWriter(new FileWriter(new File(this.filePath)));
         writer.writeAll(data);
 		writer.close();
-		System.out.println("Saved");
+	}
+	
+	public void loadCSV() throws IOException, CsvException {
+	     CSVReader reader = new CSVReader(new FileReader(this.filePath));
+	     String[] heading = reader.readNext();
+		     for(String[] line : reader) {
+		    	 System.out.println(line[0] +" "+ line[1] +" "+ line[2]);
+		     }
+		reader.close();
 	}
 
 }
